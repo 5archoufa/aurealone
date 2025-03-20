2366e6eb11d7b215397b8e5311bd0272ede1be84
5archoufa <ahmed466khemiri@gmail.com>
2025-03-20 09:31:01 +0100

    update

--- Build/aurealone-build-web.loader.js
+++ Build/aurealone-build-web.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 80928663,
+    wasmFileSize: 80925846,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
